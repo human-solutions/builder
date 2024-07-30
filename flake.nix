880564a1240d752f3f653ebb36e48c4b7825942e
anotherempty <anotherempty@outlook.com>
2024-07-30 17:49:36 +0300

    Add x86_64 linux platform

--- flake.nix
+++ flake.nix
@@ -59,5 +59,32 @@
           platforms = platforms.darwin;
         };
       };
+
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+
+      stdenvNoCC.mkDerivation rec {
+        name = "builder";
+
+        version = "0.0.3";
+
+        # https://nixos.wiki/wiki/Packaging/Binaries
+        src = pkgs.fetchurl {
+          url = "https://github.com/human-solutions/builder/releases/download/v${version}/builder-x86_64-unknown-linux-gnu.tar.xz";
+          sha256 = "sha256-o82EeaeyppnCawV5F4pJNAsUlr2TEHHnHmQDyH9Ii9k=";
+        };
+
+        sourceRoot = ".";
+
+        installPhase = ''
+        install -m755 -D builder-x86_64-unknown-linux-gnu/builder $out/bin/builder
+        '';
+
+        meta = with lib; {
+          homepage = "https://github.com/human-solutions/builder";
+          description = "Command line tool for building web assets, wasm and mobile libraries";
+          platforms = platforms.darwin;
+        };
+      };
   };
 }
