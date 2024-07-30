ab99e3981be6dc22bf6211869e039d4476f6daa2
anotherempty <anotherempty@outlook.com>
2024-07-30 17:47:17 +0300

    Add x86_64 mac platform

--- flake.nix
+++ flake.nix
@@ -9,28 +9,55 @@
     defaultPackage.aarch64-darwin =
       with import nixpkgs { system = "aarch64-darwin"; };
 
-    stdenvNoCC.mkDerivation rec {
-      name = "builder";
+      stdenvNoCC.mkDerivation rec {
+        name = "builder";
 
-      version = "0.0.3";
+        version = "0.0.3";
 
-      # https://nixos.wiki/wiki/Packaging/Binaries
-      src = pkgs.fetchurl {
-        url = "https://github.com/human-solutions/builder/releases/download/v${version}/builder-aarch64-apple-darwin.tar.xz";
-        sha256 = "sha256-o82EeaeyppnCawV5F4pJNAsUlr2TEHHnHmQDyH9Ii9k=";
+        # https://nixos.wiki/wiki/Packaging/Binaries
+        src = pkgs.fetchurl {
+          url = "https://github.com/human-solutions/builder/releases/download/v${version}/builder-aarch64-apple-darwin.tar.xz";
+          sha256 = "sha256-o82EeaeyppnCawV5F4pJNAsUlr2TEHHnHmQDyH9Ii9k=";
+        };
+
+        sourceRoot = ".";
+
+        installPhase = ''
+        install -m755 -D builder-aarch64-apple-darwin/builder $out/bin/builder
+        '';
+
+        meta = with lib; {
+          homepage = "https://github.com/human-solutions/builder";
+          description = "Command line tool for building web assets, wasm and mobile libraries";
+          platforms = platforms.darwin;
+        };
       };
 
-      sourceRoot = ".";
+    defaultPackage.x86_64-darwin =
+      with import nixpkgs { system = "x86_64-darwin"; };
 
-      installPhase = ''
-      install -m755 -D builder-aarch64-apple-darwin/builder $out/bin/builder
-      '';
+      stdenvNoCC.mkDerivation rec {
+        name = "builder";
 
-      meta = with lib; {
-        homepage = "https://github.com/human-solutions/builder";
-        description = "Command line tool for building web assets, wasm and mobile libraries";
-        platforms = platforms.darwin;
+        version = "0.0.3";
+
+        # https://nixos.wiki/wiki/Packaging/Binaries
+        src = pkgs.fetchurl {
+          url = "https://github.com/human-solutions/builder/releases/download/v${version}/builder-x86_64-apple-darwin.tar.xz";
+          sha256 = "sha256-o82EeaeyppnCawV5F4pJNAsUlr2TEHHnHmQDyH9Ii9k=";
+        };
+
+        sourceRoot = ".";
+
+        installPhase = ''
+        install -m755 -D builder-x86_64-apple-darwin/builder $out/bin/builder
+        '';
+
+        meta = with lib; {
+          homepage = "https://github.com/human-solutions/builder";
+          description = "Command line tool for building web assets, wasm and mobile libraries";
+          platforms = platforms.darwin;
+        };
       };
-    };
   };
 }
