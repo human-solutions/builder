1847f070af45129863704f89618e03687926f684
anotherempty <anotherempty@outlook.com>
2024-08-07 14:58:40 +0300

    Build wasm inside test

--- crates/builder/tests/playground.rs
+++ crates/builder/tests/playground.rs
@@ -9,7 +9,15 @@ fn test_playground() {
     let dir = Utf8PathBuf::from("../../examples/playground");
     let gen = dir.join("assets").join("gen");
 
-    // cargo(&dir, ["clean"]);
+    cargo(&dir, ["clean"]);
+    cargo(
+        &dir,
+        [
+            "build",
+            "-p=wasm",
+            "--target=wasm32-unknown-unknown",
+        ],
+    );
 
     if gen.exists() {
         fs::remove_dir_all(&gen).unwrap();
