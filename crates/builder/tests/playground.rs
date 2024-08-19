5a7fee30c7e288fba0ae515615b8bef06873abd0
anotherempty <anotherempty@outlook.com>
2024-08-19 18:32:25 +0300

    Move assets example into own workspace

--- crates/builder/tests/playground.rs
+++ crates/builder/tests/playground.rs
@@ -5,6 +5,72 @@ use common::{cargo, PathExt, Replacer};
 use fs_err as fs;
 
 #[test]
+fn test_assets() {
+    let dir = Utf8PathBuf::from("../../examples/assets-wksp");
+    let gen = dir.join("assets").join("gen");
+
+    cargo(&dir, ["clean"]);
+    if gen.exists() {
+        fs::remove_dir_all(&gen).unwrap();
+    }
+
+    cargo(&dir, ["build"]);
+
+    insta::assert_snapshot!(gen.ls_ascii_replace::<NoChange>(0).unwrap(), @r###"
+    gen:
+      mobile.rs
+      web.rs
+    "###);
+
+    cargo(&dir, ["build", "--release"]);
+
+    let out = dir.join("target").join("assets");
+
+    insta::assert_snapshot!(out.ls_ascii_replace::<RemoveTargetAndChecksum>(0).unwrap(), @r###"
+    assets:
+      prebuild-debug.log
+      prebuild-release.log
+      <target>:
+        mobile:
+          debug:
+            main.css
+            static:
+              <checksum>polyglot.woff2
+          release:
+            main.css.br
+            static:
+              hfT-f2u761M=polyglot.woff2.br
+              hfT-f2u761M=polyglot.woff2.gz
+        web:
+          debug:
+            static:
+              <checksum>main.css
+              <checksum>polyglot.woff2
+              badge:
+                static:
+                  badge:
+                    MJjU0sjYbCw=apple_store.svg.en
+                    MJjU0sjYbCw=apple_store.svg.fr
+                    MJjU0sjYbCw=apple_store.svg.fr-CA
+          release:
+            static:
+              4xved-FTXA0=main.css.br
+              4xved-FTXA0=main.css.gz
+              hfT-f2u761M=polyglot.woff2.br
+              hfT-f2u761M=polyglot.woff2.gz
+              badge:
+                static:
+                  badge:
+                    MJjU0sjYbCw=apple_store.svg.en.br
+                    MJjU0sjYbCw=apple_store.svg.en.gz
+                    MJjU0sjYbCw=apple_store.svg.fr-CA.br
+                    MJjU0sjYbCw=apple_store.svg.fr-CA.gz
+                    MJjU0sjYbCw=apple_store.svg.fr.br
+                    MJjU0sjYbCw=apple_store.svg.fr.gz
+    "###);
+}
+
+#[test]
 fn test_playground() {
     let dir = Utf8PathBuf::from("../../examples/playground");
     let gen = dir.join("assets").join("gen");
