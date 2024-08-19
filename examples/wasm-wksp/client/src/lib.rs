c852145f7ec5d43c17177ce06fad602c6fc60e7a
anotherempty <anotherempty@outlook.com>
2024-08-19 19:28:17 +0300

    Move wasm example into own workspace

---
+++ examples/wasm-wksp/client/src/lib.rs
@@ -0,0 +1,19 @@
+// This would normally have the below inclueds, but
+// when building on CI, the builder is not available, sot
+// the builder is skipped in the build.rs file, and thus
+// these files are not generated
+
+// include!("../gen/mobile.rs");
+// include!("../gen/web.rs");
+
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
