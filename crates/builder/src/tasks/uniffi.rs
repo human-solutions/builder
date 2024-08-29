0f3dcbc29e7d84ea09912a0ef2842eca0da6d29a
anotherempty <anotherempty@outlook.com>
2024-08-29 15:36:21 +0300

    Add uniffi processing to tasks

---
+++ crates/builder/src/tasks/uniffi.rs
@@ -0,0 +1,70 @@
+use std::fmt;
+
+use anyhow::Result;
+use camino::Utf8PathBuf;
+use serde::{Deserialize, Serialize};
+use uniffi_bindgen::{
+    bindings::{KotlinBindingGenerator, SwiftBindingGenerator},
+    generate_external_bindings,
+};
+
+use crate::{anyhow::Context, generate::Output};
+
+use super::Config;
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub(super) enum UniffiLanguage {
+    #[default]
+    #[serde(rename = "kotlin")]
+    Kotlin,
+    #[serde(rename = "swift")]
+    Swift,
+}
+
+impl fmt::Display for UniffiLanguage {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            UniffiLanguage::Kotlin => write!(f, "Kotlin"),
+            UniffiLanguage::Swift => write!(f, "Swift"),
+        }
+    }
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub(super) struct UniffiParams {
+    pub udl_path: Utf8PathBuf,
+    pub language: UniffiLanguage,
+    pub out: Output,
+}
+
+impl UniffiParams {
+    pub fn process(&self, config: &Config) -> Result<()> {
+        let out_folder = self.out.folder.as_deref().unwrap_or("".into());
+        let out_dir = config.site_dir("uniffi").join(out_folder);
+
+        match self.language {
+            UniffiLanguage::Kotlin => generate_external_bindings(
+                &KotlinBindingGenerator,
+                &self.udl_path,
+                None::<&Utf8PathBuf>,
+                Some(out_dir),
+                None::<&Utf8PathBuf>,
+                Some(&config.package_name),
+                true,
+            ),
+            UniffiLanguage::Swift => generate_external_bindings(
+                &SwiftBindingGenerator,
+                &self.udl_path,
+                None::<&Utf8PathBuf>,
+                Some(out_dir),
+                None::<&Utf8PathBuf>,
+                Some(&config.package_name),
+                true,
+            ),
+        }
+        .context(format!(
+            "Failed to generate {} bindings for {}",
+            self.language, config.package_name
+        ))
+    }
+}
