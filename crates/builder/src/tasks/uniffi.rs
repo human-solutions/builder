177654b05d0d2bdc9bd50be345b778e62f01cc95
anotherempty <anotherempty@outlook.com>
2024-09-17 14:06:15 +0300

    Use object file when creating bindings

--- crates/builder/src/tasks/uniffi.rs
+++ crates/builder/src/tasks/uniffi.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{env, fmt};
 
 use anyhow::Result;
 use camino::Utf8PathBuf;
@@ -38,6 +38,8 @@ pub(super) struct UniffiParams {
 
 impl UniffiParams {
     pub fn process(&self, config: &Config) -> Result<()> {
+        const DEFAULT_PROFILE: &str = "debug";
+
         let out_folder = self.out.folder.as_deref().unwrap_or("".into());
         let out_dir = config.site_dir("uniffi").join(out_folder);
 
@@ -46,13 +48,37 @@ impl UniffiParams {
             .dir
             .join(format!("src/{}.udl", config.package_name));
 
+        let profile = if config.args.profile.is_empty() {
+            DEFAULT_PROFILE.to_string()
+        } else {
+            env::var("OUT_DIR")
+                .context("Failed to get OUT_DIR environment variable")?
+                .split(std::path::MAIN_SEPARATOR)
+                .nth_back(3)
+                .unwrap_or(DEFAULT_PROFILE)
+                .to_string()
+        };
+
+        let profile = if profile == "release" {
+            "lib-release".to_owned()
+        } else {
+            profile
+        };
+
+        let is_mac = cfg!(target_os = "macos");
+        let ext = if is_mac { "dylib" } else { "so" };
+
+        let library_file = config
+            .target_dir
+            .join(format!("{}/lib{}.{}", profile, config.package_name, ext));
+
         match self.language {
             UniffiLanguage::Kotlin => generate_external_bindings(
                 &KotlinBindingGenerator,
                 &udl_file,
                 None::<&Utf8PathBuf>,
                 Some(out_dir),
-                None::<&Utf8PathBuf>,
+                Some(library_file),
                 Some(&config.package_name),
                 true,
             ),
@@ -61,7 +87,7 @@ impl UniffiParams {
                 &udl_file,
                 None::<&Utf8PathBuf>,
                 Some(out_dir),
-                None::<&Utf8PathBuf>,
+                Some(library_file),
                 Some(&config.package_name),
                 true,
             ),
