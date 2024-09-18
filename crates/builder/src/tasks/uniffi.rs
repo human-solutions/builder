e6394a55e17178a085f9c22bced45b275c4b7f1d
anotherempty <102887725+anotherempty@users.noreply.github.com>
2024-09-18 08:27:23 +0300

    Use package name to determine udl file path (#29)
    * Use package name to determine udl file path
    
    * Use object file when creating bindings
    
    * Bump version

--- crates/builder/src/tasks/uniffi.rs
+++ crates/builder/src/tasks/uniffi.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{env, fmt};
 
 use anyhow::Result;
 use camino::Utf8PathBuf;
@@ -32,17 +32,45 @@ impl fmt::Display for UniffiLanguage {
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub(super) struct UniffiParams {
-    #[serde(rename = "udl-path")]
-    pub udl_path: Utf8PathBuf,
     pub language: UniffiLanguage,
     pub out: Output,
 }
 
 impl UniffiParams {
     pub fn process(&self, config: &Config) -> Result<()> {
+        const DEFAULT_PROFILE: &str = "debug";
+
         let out_folder = self.out.folder.as_deref().unwrap_or("".into());
         let out_dir = config.site_dir("uniffi").join(out_folder);
-        let udl_file = config.args.dir.join(&self.udl_path);
+
+        let udl_file = config
+            .args
+            .dir
+            .join(format!("src/{}.udl", config.package_name));
+
+        let profile = if config.args.profile.is_empty() {
+            DEFAULT_PROFILE.to_string()
+        } else {
+            env::var("OUT_DIR")
+                .context("Failed to get OUT_DIR environment variable")?
+                .split(std::path::MAIN_SEPARATOR)
+                .nth_back(3)
+                .unwrap_or(DEFAULT_PROFILE)
+                .to_string()
+        };
+
+        let profile = if profile == "release" {
+            "lib-release".to_owned()
+        } else {
+            profile
+        };
+
+        let is_mac = cfg!(target_os = "macos");
+        let ext = if is_mac { "dylib" } else { "so" };
+
+        let library_file = config
+            .target_dir
+            .join(format!("{}/lib{}.{}", profile, config.package_name, ext));
 
         match self.language {
             UniffiLanguage::Kotlin => generate_external_bindings(
@@ -50,7 +78,7 @@ impl UniffiParams {
                 &udl_file,
                 None::<&Utf8PathBuf>,
                 Some(out_dir),
-                None::<&Utf8PathBuf>,
+                Some(library_file),
                 Some(&config.package_name),
                 true,
             ),
@@ -59,7 +87,7 @@ impl UniffiParams {
                 &udl_file,
                 None::<&Utf8PathBuf>,
                 Some(out_dir),
-                None::<&Utf8PathBuf>,
+                Some(library_file),
                 Some(&config.package_name),
                 true,
             ),
