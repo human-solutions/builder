8bf9a177c433d6b6ac3b02dfe7803df64e562d94
anotherempty <anotherempty@outlook.com>
2024-09-26 22:17:51 +0300

    Add env variable to optionally output to stdout for build script

--- crates/builder/src/lib.rs
+++ crates/builder/src/lib.rs
@@ -5,22 +5,35 @@ mod install;
 mod tasks;
 mod util;
 
+use std::env;
+
 pub use commands::{CmdArgs, Commands};
 pub use ext::anyhow;
 use fs_err::File;
 use log::LevelFilter;
 use simplelog::{
-    ColorChoice, CombinedLogger, Config as LogConfig, TermLogger, TerminalMode, WriteLogger,
+    format_description, ColorChoice, CombinedLogger, Config as LogConfig, ConfigBuilder,
+    TermLogger, TerminalMode, WriteLogger,
 };
 
 pub fn setup_logging(log_file: &str, log_level: LevelFilter) {
-    const LOG_TO_TERM_AND_FILE: bool = false;
+    const LOG_TO_TERM_AND_FILE: bool = true;
 
     if LOG_TO_TERM_AND_FILE {
+        let log_config = if env::var("BUILDER_LOG_AS_CARGO_WARNING").is_ok() {
+            ConfigBuilder::new()
+                .set_time_format_custom(format_description!(
+                    "cargo::warning=[hour]:[minute]:[second]"
+                ))
+                .build()
+        } else {
+            LogConfig::default()
+        };
+
         let _ = CombinedLogger::init(vec![
             TermLogger::new(
                 log_level,
-                LogConfig::default(),
+                log_config,
                 TerminalMode::Mixed,
                 ColorChoice::Auto,
             ),
